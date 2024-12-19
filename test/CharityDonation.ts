6c47087bb1e65c0c3ada61a5eb702dc40111cae7
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2024-12-19 23:49:20 +0300

    added the charity-donation test file

---
+++ test/CharityDonation.ts
@@ -0,0 +1,10 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { ethers } from "hardhat"
+import { expect } from 'chai'
+
+describe("CharityDonation", () => {
+    // Definination of a fixture to reuse the same setup in every test.
+    async function testCampaign() {
+        
+    }
+});
